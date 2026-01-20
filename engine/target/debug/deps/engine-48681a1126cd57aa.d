4bf3d51b69618bf00e0fb37b6d4f6429a7b8598a
alicinararik <ytalcin01@gmail.com>
2026-01-21 00:25:00 +0300

    Created needed Rust files via Cargo.

---
+++ engine/target/debug/deps/engine-48681a1126cd57aa.d
@@ -0,0 +1,5 @@
+C:\Users\Ali\Desktop\Open Source\opensource-engine\engine\target\debug\deps\engine-48681a1126cd57aa.d: src\main.rs
+
+C:\Users\Ali\Desktop\Open Source\opensource-engine\engine\target\debug\deps\libengine-48681a1126cd57aa.rmeta: src\main.rs
+
+src\main.rs:
---
+++ engine/target/debug/deps/engine-ff1294a3f332b2a2.d
@@ -0,0 +1,5 @@
+C:\Users\Ali\Desktop\Open Source\opensource-engine\engine\target\debug\deps\engine-ff1294a3f332b2a2.d: src\main.rs
+
+C:\Users\Ali\Desktop\Open Source\opensource-engine\engine\target\debug\deps\libengine-ff1294a3f332b2a2.rmeta: src\main.rs
+
+src\main.rs:
